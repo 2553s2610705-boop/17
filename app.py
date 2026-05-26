0ea40104f211bbd82af49c101b16a08ddd2f3d79
김가람 <2553s2610705@g.cnees.kr>
2026-05-26 10:18:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
-st.title('나는 김가람이다')
-st.write('김가람은 똑똑하다')
+import random
+
+colors = [
+    "빨간색 ❤️",
+    "파란색 💙",
+    "초록색 💚",
+    "노란색 💛",
+    "보라색 💜",
+    "주황색 🧡",
+    "핑크색 🩷",
+    "하늘색 🩵"
+]
+
+st.title("🎨 오늘의 행운 색깔")
+
+if st.button("행운 색깔 확인하기"):
+    lucky_color = random.choice(colors)
+    st.success(f"오늘의 행운 색깔은 {lucky_color} 입니다!")
